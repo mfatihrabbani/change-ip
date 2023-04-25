b20783dc9f67ab075ddb8a5af2262154d259c8a9
mfatihrabbani <mfatihrabbani@gmail.com>
2023-04-25 13:11:32 +0700

    error handling

--- src/services/ipServices.js
+++ src/services/ipServices.js
@@ -61,13 +61,7 @@ export const changeIp = async (fileName) => {
     if (fileName == null){
         validationError("Parameter cannot null")
     }
-    await exec(`Start-Process -Verb RunAs powershell.exe -Args '-ExecutionPolicy Bypass -command wireguard /installtunnelservice \\""C:\\Program Files\\WireGuard\\Data\\Configurations\\${fileName}.conf.dpapi"\"'`, {'shell':'powershell.exe'},(error, stdout, stderr) => {
-        if (error == null){
-            console.log("Success change IP")
-            return 'Success change IP'
-        }
-        console.log(stderr)
-        console.log("Failed change IP")
-        throw validationError("Failed to change IP")
-    })
+    const shutdown = await shutdownCurrentConfig(fileName)
+    const change = await changeConfig(fileName)
+    return change
 }
